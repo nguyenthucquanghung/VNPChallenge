0c3d1ce58c2dd776d02271eb49bdc76c6c74e954
thienanh1999 <thienanh313@gmail.com>
2019-06-15 04:09:09 -0700

    create order fragment ui

---
+++ app/src/main/java/com/example/vnpchallenge/screen/CreateOrderFragment.java
@@ -0,0 +1,30 @@
+package com.example.vnpchallenge.screen;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.vnpchallenge.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CreateOrderFragment extends Fragment {
+
+
+    public CreateOrderFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_order, container, false);
+    }
+
+}
