7807d5deb77aa00f47081912d1e21b1736ded7a9
thienanh1999 <thienanh313@gmail.com>
2019-06-15 10:59:34 -0700

    add logout

--- app/src/main/java/com/example/vnpchallenge/screen/UserFragment.java
+++ app/src/main/java/com/example/vnpchallenge/screen/UserFragment.java
@@ -1,13 +1,19 @@
 package com.example.vnpchallenge.screen;
 
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.vnpchallenge.R;
+import com.example.vnpchallenge.base.Constant;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,4 +33,23 @@ public class UserFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_user, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btnLogout = view.findViewById(R.id.btn_logout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences = getContext().getSharedPreferences(Constant.USER, getContext().MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(Constant.LOGIN, null);
+                editor.apply();
+
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                getActivity().startActivity(intent);
+                getActivity().finish();
+            }
+        });
+    }
 }
